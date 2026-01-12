4f52b314e760b87a3805ffba386ec6cd94cad455
stevedarrish-stack <stevedarrish@yahoo.com>
2026-01-12 16:35:37 -0600

    Add app.py Flask entry point for Render

--- New_Site_WyattJoel/app.py
+++ New_Site_WyattJoel/app.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return "Hello, world!"from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
     return "Hello, world!"from flask import Flask, request
 
 
